664458a2523a2562b5c9898c2dc4e73473f5397e
3manifold <22544721+3manifold@users.noreply.github.com>
2026-02-05 10:17:23 +0100

    Check for both "APOSSTEL" and "APOSSEL" in test_transformers_wav2vec2

--- python/tests/test_transformers.py
+++ python/tests/test_transformers.py
@@ -977,12 +977,14 @@ class TestWav2Vec2:
     @test_utils.only_on_linux
     @test_utils.on_available_devices
     @pytest.mark.parametrize(
-        "model_name,expected_transcription",
+        "model_name,expected_transcriptions",
         [
             (
                 "facebook/wav2vec2-large-robust-ft-swbd-300h",
                 [
                     "MISTER QUILTER IS THE APOSSEL OF THE MIDDLE CLASSES AND"
+                    " WE ARE GLAD TO WELCOME HIS GOSPEL",
+                    "MISTER QUILTER IS THE APOSSTEL OF THE MIDDLE CLASSES AND"
                     " WE ARE GLAD TO WELCOME HIS GOSPEL",
                 ],
             ),
@@ -993,7 +995,7 @@ class TestWav2Vec2:
         tmp_dir,
         device,
         model_name,
-        expected_transcription,
+        expected_transcriptions,
     ):
         import torch
         import transformers
@@ -1046,7 +1048,7 @@ class TestWav2Vec2:
         transcription = processor.decode(predicted_ids, output_word_offsets=True)
         transcription = transcription[0].replace(processor.tokenizer.unk_token, "")
 
-        assert transcription == expected_transcription[0]
+        assert transcription in expected_transcriptions
 
 
 class TestWav2Vec2Bert:
